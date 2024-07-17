3a9635d717a46ec0ff77b34532e9688eae45ab5d
somin-s <sarawoot.somin@gmail.com>
2024-07-17 22:39:25 +1200

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
     production: false,
     APIUrl: "http://localhost:3000/api/",
     APIUrlFlask: "http://127.0.0.1:5000/"
+    //APIUrl:"https://wine-analytic.onrender.com/api/",
+    //APIUrlFlask: "https://python-api-8m03.onrender.com/",
     //APIUrl: "http://localhost:5277/api/Pinot/"
 };
