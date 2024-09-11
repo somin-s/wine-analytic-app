844aeaa410dadfd674c07b9b9b5307e754ac9167
somin-s <sarawoot.somin@gmail.com>
2024-09-11 14:32:06 +1200

    Fast2

--- src/app/arc-gis/arc-gis.component.ts
+++ src/app/arc-gis/arc-gis.component.ts
@@ -25,6 +25,34 @@ export class ArcGisComponent {
 
   constructor(private service: ServiceService, private dialog: MatDialog) { } 
 
+  ngOnInit(): void{
+    //this.genMap("All"); to make it faster
+
+    this.loaded = true;
+    const data = [{Vineyard:"Clayvin Single Vineyard",Region:"Marlborough",Producer:"Giesen 13",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/502/321.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Pencarrow",Region:"Wairarapa",Producer:"Palliser 13",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/501/321.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Bendigo",Region:"Central Otago",Producer:"Quartz Reef 13",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/500/321.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"McCrone",Region:"Wairarapa",Producer:"Ata Rangi",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/505/319.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Clos Henri",Region:"Marlborough",Producer:"Clos Henri",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/505/319.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Aroha",Region:"Wairarapa",Producer:"Craggy Range",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/505/318.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Escarpment",Region:"Wairarapa",Producer:"Escarpment",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/505/317.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Block 3",Region:"Central Otago",Producer:"Felton Road",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/505/316.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Cornish Point",Region:"Central Otago",Producer:"Felton Road",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/506/316.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Clayvin",Region:"Marlborough",Producer:"Giesen",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/505/316.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Greystone",Region:"N. Canterbury",Producer:"Greystone",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/504/316.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Roaring Meg",Region:"Central Otago",Producer:"Mt. Difficulty",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/505/320.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Moutere",Region:"Nelson",Producer:"Neudorf",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/503/320.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Pencarrow",Region:"Wairarapa",Producer:"Palliser",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/504/317.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Main Divide",Region:"N. Canterbury",Producer:"Pegasus Bay",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/506/315.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Stoneleigh",Region:"N. Canterbury",Producer:"Pernod Ricard",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/507/315.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Bendigo",Region:"Central Otago",Producer:"Quartz Reef",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/508/315.png?api=edf8fdb7896c4081bf79b2c831a5758e"},
+    {Vineyard:"Old Coach Rd",Region:"Nelson",Producer:"Siefried",Layer_ID1:"https://basemaps.linz.govt.nz/v1/tiles/aerial/3857/9/507/316.png?api=edf8fdb7896c4081bf79b2c831a5758e"},];
+    this.Producers = data;
+    this.dataSource = new MatTableDataSource(data);
+  }
+
+  loaded = false;
+
   onChange() {
     if(this.toggleLabel.value == 'Show filter') {
        this.toggleLabel.setValue('Hide filter');
@@ -37,6 +65,7 @@ export class ArcGisComponent {
   onChangeGIS(){
     var region: string = this.selectedGIS.value;
     if (region != "All"){
+      this.loaded = false;
       this.genMap(region);
       console.log(region);
     }
@@ -49,21 +78,20 @@ export class ArcGisComponent {
   private genMap(str: string): void {
 
     if (str == "All"){
-      this.service.getMetadata().subscribe(data=>{
-        this.Producers = data;
-        this.dataSource = new MatTableDataSource(data);
-      });
+      // this.service.getMetadata().subscribe(data=>{
+      //   this.Producers = data;
+      //   this.dataSource = new MatTableDataSource(data);
+      // });
+      this.ngOnInit()
     }
     else{
+      this.loaded = false;
       this.service.gerMetadataBProduction(str).subscribe(data=>{
+        this.loaded = true;
         this.Producers = data;
       });
       
     }
-  }
-
-  ngOnInit(): void{
-    this.genMap("All");
   }
 
   onDetail(producer: string, vinyard: string): void{
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,10 @@
 export const environment = {
     production: false,
-    //APIUrl: "http://localhost:3000/api/", //for NodeJs
-    //APIUrlFlask: "http://127.0.0.1:5000/", //for flask
+    APIUrl: "http://localhost:3000/api/", //for NodeJs
+    APIUrlFlask: "http://127.0.0.1:5000/", //for flask
     //APIUrl: "http://localhost:5277/api/Pinot/" // for .Net
 
-    APIUrl:"https://wine-analytic.onrender.com/api/",
-    APIUrlFlask: "https://python-api-8m03.onrender.com/",
+    //from cloud
+    // APIUrl:"https://wine-analytic.onrender.com/api/",
+    // APIUrlFlask: "https://python-api-8m03.onrender.com/",
 };
